d829bac638f6cc0db285d3ba36aab04f06351344
TyrenK <bizinottoluca@gmail.com>
2025-08-20 09:54:06 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AcessScreen from './src/components/AcessScreen';
 import LoginScreen from './src/components/LoginScreen';
 
 const Stack = createStackNavigator();
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Acess" component={AcessScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
